2216c928a05b2660276550237e78d81fd9fb5905
ydoudothis <mail@relatedpixels.com>
2016-12-07 10:48:36 +0100

    change startup script

--- ApplicationStart.sh
+++ ApplicationStart.sh
@@ -1,2 +1,1 @@
-cd /home/node-js/rss
-sudo NODE_ENV=production pm2 startOrRestart server.js --log-date-format="YYYY-MM-DD HH:mm Z"
+sudo NODE_ENV=production pm2 startOrRestart server  --log-date-format="YYYY-MM-DD HH:mm Z"
