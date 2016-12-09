6c29dea7684c8698fd89e0551d86b35dafe596f5
ydoudothis <mail@relatedpixels.com>
2016-12-09 11:01:54 +0100

    test

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ Emaileditor.register(function(app, auth, database, circles)
    });  
 
    Emaileditor.menus.add({
-    title: 'Entities1',
+    title: 'Entities',
     link: 'newsletter overview',
     roles: ['authenticated'],
     weight: ['Company_Admin'],
